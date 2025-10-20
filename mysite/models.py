ea175dcf6eeb71fe57c730390d113ded92063587
Stefanie <marystefaniebermudez@gmail.com>
2025-10-20 08:06:42 -0500

    Refactor models and templates for improved structure; add Categoria model and clean up base.html

--- mysite/models.py
+++ mysite/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User #usuario autenticado
 
 # Create your models here.
+
+#las catergorias de los productos
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
@@ -18,7 +27,8 @@ class Producto(models.Model):
         self.save()
     def __str__(self):
         return self.nombre
-from django.contrib.auth.models import User
+    
+
 
 class Cliente(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
